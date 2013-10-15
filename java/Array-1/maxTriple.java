9d8675cc434ac61ce3472d196933db244debec87
Ramesh <rameshkopparapu@gmail.com>
2013-10-15 21:56:02 +0530

    Update maxTriple.java
    Just reduced it by a line. every line matters :)

--- java/Array-1/maxTriple.java
+++ java/Array-1/maxTriple.java
@@ -1,5 +1,4 @@
 public int maxTriple(int[] nums) {
   int largest = Math.max(nums[0], nums[nums.length / 2]);
-  largest = Math.max(largest, nums[nums.length-1]);
-  return largest;
+  return Math.max(largest, nums[nums.length-1]);
 }
